b256a218fe9bbaabd1d1c22cf8f8f4edebde3e9f
Vineeth Pillai <viremana@linux.microsoft.com>
2021-02-24 10:45:45 -0500

    ch_monitor: Kill the cloud-hypervisor procees on monitor close
    commit 34b9c26609 modified the VM creation by converting the
    cloud-hypervisor as a daemon there by reparenting to init. So
    we can no longer use virProcessAbort as it assumes that the
    process to be killed is a child.
    
    So use virProcessKillPainfully instead.
    
    Co-Authored-by: Vineeth Pillai <viremana@linux.microsoft.com>
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -723,7 +723,7 @@ void virCHMonitorClose(virCHMonitor *mon)
 
     if (mon->pid > 0) {
         /* try cleaning up the Cloud-Hypervisor process */
-        virProcessAbort(mon->pid);
+        virProcessKillPainfully(mon->pid, true);
         mon->pid = 0;
     }
 
