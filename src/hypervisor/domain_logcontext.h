a76de802f6c282800130b027a54135caa633d1ca
Praveen K Paladugu <praveenkpaladugu@gmail.com>
2024-12-03 16:33:55 -0600

    hypervisor: drop hypervisor specific args in domainLogContextNew
    Drop Qemu specific arguments from domainLogContextNew.
    
    Signed-off-by: Praveen K Paladugu <praveenkpaladugu@gmail.com>

--- src/hypervisor/domain_logcontext.h
+++ src/hypervisor/domain_logcontext.h
@@ -21,12 +21,16 @@
 #include <glib-object.h>
 #include "logging/log_manager.h"
 #include "virconftypes.h"
+#include "domain_conf.h"
 
 #define DOMAIN_TYPE_LOG_CONTEXT domain_log_context_get_type()
 G_DECLARE_FINAL_TYPE(domainLogContext, domain_log_context, DOMAIN, LOG_CONTEXT, GObject);
 
-domainLogContext *domainLogContextNew(virQEMUDriver *driver,
+domainLogContext *domainLogContextNew(bool stdioLogD,
+                                      char *logDir,
+                                      const char *driver_name,
                                       virDomainObj *vm,
+                                      bool privileged,
                                       const char *basename);
 int domainLogContextWrite(domainLogContext *ctxt,
                           const char *fmt, ...) G_GNUC_PRINTF(2, 3);
