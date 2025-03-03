e43d3a6456ebcf7ba0dc12a3ec37bf3a24d8a6a8
Praveen K Paladugu <praveenkpaladugu@gmail.com>
2025-03-03 16:04:38 -0600

    ch: allow block devices as disk sources
    Allow block devices to be specified as sources for virtual disks.
    
    Signed-off-by: Praveen K Paladugu <praveenkpaladugu@gmail.com>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -243,6 +243,7 @@ virCHMonitorBuildDiskJson(virJSONValue *disks, virDomainDiskDef *diskdef)
         return -1;
 
     switch (diskdef->src->type) {
+    case VIR_STORAGE_TYPE_BLOCK:
     case VIR_STORAGE_TYPE_FILE:
         if (!diskdef->src->path) {
             virReportError(VIR_ERR_INVALID_ARG, "%s",
@@ -266,7 +267,6 @@ virCHMonitorBuildDiskJson(virJSONValue *disks, virDomainDiskDef *diskdef)
 
         break;
     case VIR_STORAGE_TYPE_NONE:
-    case VIR_STORAGE_TYPE_BLOCK:
     case VIR_STORAGE_TYPE_DIR:
     case VIR_STORAGE_TYPE_NETWORK:
     case VIR_STORAGE_TYPE_VOLUME:
