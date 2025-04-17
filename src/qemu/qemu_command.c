49bdb6b2b75087db14365654f4ffc562dff4f180
Praveen K Paladugu <prapal@linux.microsoft.com>
2025-04-17 21:37:48 +0000

    qemu: add mshv accel for HYPERV Hypervisor
    Dont pass acceleration flags to qemu process
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -7321,6 +7321,9 @@ qemuBuildAccelCommandLine(virCommand *cmd,
         virBufferAddLit(&buf, "hvf");
         break;
 
+    case VIR_DOMAIN_VIRT_HYPERV:
+        virBufferAddLit(&buf, "mshv");
+        break;
     case VIR_DOMAIN_VIRT_KQEMU:
     case VIR_DOMAIN_VIRT_XEN:
     case VIR_DOMAIN_VIRT_LXC:
@@ -7328,7 +7331,6 @@ qemuBuildAccelCommandLine(virCommand *cmd,
     case VIR_DOMAIN_VIRT_OPENVZ:
     case VIR_DOMAIN_VIRT_TEST:
     case VIR_DOMAIN_VIRT_VMWARE:
-    case VIR_DOMAIN_VIRT_HYPERV:
     case VIR_DOMAIN_VIRT_VBOX:
     case VIR_DOMAIN_VIRT_PHYP:
     case VIR_DOMAIN_VIRT_PARALLELS:
